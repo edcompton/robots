ad34f4b09e0f83400f045419b686726a232d73d0
Ed Compton <ed.compton@opearlo.com>
2018-06-23 11:31:33 +0100

    grab coordinates and robot instructions from the command line and log out arrays

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const game = {};
 
 // First off, initialise the exercise by grabbing the inputs given on the command line and storing them in variables for later.
 game.initialise = function() {
-  
+  game.isLost = false;
+  // Store the arguments from the command line in variables to be passed to setup functions in due course. Convert gridSize to an integer for ease of later board creation (if visualising).
+  game.gridCoords = game.setCoordinates(process.argv.slice(2, 3)[0]);
+
+  game.inputs = process.argv.slice(3, process.argv.length);
+
+  console.log(game.gridCoords);
+  console.log(game.inputs);
 };
