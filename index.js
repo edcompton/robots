3eaea7e7348a70276d6dab39d844d7e56cbbf3e9
Ed Compton <ed.compton@opearlo.com>
2018-06-24 17:53:25 +0100

    added some error handling on inputs

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const readline = require('readline');
 
 const game = {};
 
-game.isLost = false;
-game.finalRobotPositions = [];
-game.lostCoordinates = [];
-game.robotPositions = {};
-
+// Function to gather inputs from the command line and execute program on 'run' input
 function gatherInputs() {
+  game.isLost = false;
+  game.finalRobotPositions = [];
+  game.lostCoordinates = [];
+  game.robotPositions = {};
+
   var rl = readline.createInterface(process.stdin, process.stdout);
   let counter = 0;
   rl.setPrompt('> ');
@@ -19,23 +20,32 @@ function gatherInputs() {
     if (line === 'run') {
       findFinalPositions.call(game);
       process.exit(0);
+
     } else if (line && counter === 0) {
-      counter++;
       const grid = line.split(' ');
-      game.gridCoords = { x: grid[0], y: grid[1] };
 
-    } else if (line && !line.match(/[0-9]/)) {
-      game.robotPositions[counter].instructions = line.split('');
-      counter++;
+      if (grid[0] > 50 || grid[1] > 50) {
+        console.log('Each axis value must not be greater than 50');
+        rl.prompt();
+      } else {
+        game.gridCoords = { x: grid[0], y: grid[1] };
+        counter++;
+      }
+    } else if ((line && /[A-Z]/.test(line.split(' ')[0])) && game.robotPositions[counter]) {
+      const instructions = line.split('');
 
-    } else if (line) {
+      if (instructions.length > 99) {
+        console.log('Robot instructions must be less than 100 characters');
+        rl.prompt();
+      } else {
+        game.robotPositions[counter].instructions = instructions;
+        counter++;
+      }
+
+    } else if (line && /[0-9]/.test(line.split(' ')[0])) {
       const input = line.split(' ');
       game.robotPositions[counter] = {
-        startingCoord: {
-          x: parseInt(input[0]),
-          y: parseInt(input[1]),
-          direction: input[2]
-        }
+        startingCoord: { x: parseInt(input[0]), y: parseInt(input[1]), direction: input[2] }
       };
     }
 
